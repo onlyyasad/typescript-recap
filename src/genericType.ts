bafeed855102d9d90af4504b8cc6707ef5c20f3a
hafij <hafij.alasad23@gmail.com>
2025-02-18 00:24:49 +0600

    feat: generic type recap

---
+++ src/genericType.ts
@@ -0,0 +1,10 @@
+{
+    type GenericArray<T> = Array<T>;
+
+    const usersName: GenericArray<string> = ["Habib", "Rahim", "Karim"];
+
+    const usersAge: GenericArray<number> = [20, 45, 65];
+
+    const usersMaritalStatus: GenericArray<boolean> = [true, false, true];
+
+}
